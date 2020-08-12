f6c2915d971971ee6917b3074ecc1114ed7ee351
Igor Prilepov <sliceyounice@gmail.com>
2020-08-12 15:17:55 +0300

    added proxy

--- bot/message_handlers.py
+++ bot/message_handlers.py
@@ -1,10 +1,8 @@
 from mongo import users as users
 
-
 def start(update, context):
     if users.save({'chatId': update.message.chat['id'], 'username': update.message.chat['username']}):
         message = "Теперь тебе будут приходить уведомления о квартирах в Донецке"
-
     else:
         message = "Ты уже подписан на обновления"
     update.message.reply_text(message)
--- parser_functions/__init__.py
+++ parser_functions/__init__.py
@@ -2,6 +2,7 @@ import requests
 from lxml import html as parser
 from mongo import offers
 import itertools
+from decouple import config
 
 
 def construct_offer(node):
@@ -19,9 +20,12 @@ def construct_offer(node):
 
 def parse_offers():
     data = {'search[city_id]': (None, 141), 'search[region_id]': (None, 4), 'search[category_id]': (None, 1147),
-            'search[filter_float_number_of_rooms:from]': (None, 1), 'search[filter_float_number_of_rooms:to]': (None, 1)}
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-    response = requests.post('https://www.olx.ua/ajax/donetsk/search/list/', files=data, headers={"User-Agent": user_agent})
+            'search[filter_float_number_of_rooms:from]': (None, 1),
+            'search[filter_float_number_of_rooms:to]': (None, 1)}
+    proxy = {
+        "https": config('proxy')
+    }
+    response = requests.post('https://www.olx.ua/ajax/donetsk/search/list/', files=data, proxies=proxy, verify=False)
     offer_nodes = parser.fromstring(response.content.decode('utf-8')).cssselect('table#offers_table>tbody>tr.wrap')
     return [construct_offer(node) for node in offer_nodes]
 
